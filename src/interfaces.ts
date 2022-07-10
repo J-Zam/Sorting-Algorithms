7a2b14c6fc035f2986f0636f98ca879efbc1fb06
Zambrano <51100407+J-Zam@users.noreply.github.com>
2022-07-10 00:49:19 +0000

    feat: buttons blocked

---
+++ src/interfaces.ts
@@ -0,0 +1,5 @@
+export interface IDimensions {
+  key: number,
+  widthBar: number,
+  heightBar: number
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.scss'
+import { IDimensions } from "./interfaces";
 
 let randomizeArray: HTMLElement | null = document.getElementById("random_btn");
 let sortBtn: HTMLElement | null = document.getElementById("sort_btn");
@@ -7,14 +8,18 @@ let barsContainer: HTMLElement | null = document.getElementById("bars_container"
 let slider: HTMLInputElement | null = (document.getElementById("slider") as HTMLInputElement);
 let speed = document.getElementById("speed");
 let numsOfBars = parseInt(slider!.value);
-let heightFactor = 5;
+let heightFactor = 40;
 let speedFactor = 100;
+let widthBar = 60;
+let dimensions: IDimensions[] = [];
 let unsortedArray = new Array(numsOfBars);
 let comparisons: HTMLElement | null = document.getElementById("comparisons")
 let algorithmToUse = "";
 
+
 document.addEventListener("DOMContentLoaded", function () {
   unsortedArray = createRandomArray();
+  generateDimensions();
   renderBars(unsortedArray);
 });
 
@@ -22,6 +27,7 @@ slider.addEventListener("input", () => {
   numsOfBars = parseInt(slider!.value);
   unsortedArray = createRandomArray();
   barsContainer!.innerHTML = "";
+  comparisons!.textContent = "";
   renderBars(unsortedArray);
 });
 
@@ -51,6 +57,19 @@ sortBtn?.addEventListener("click", () => {
   }
 });
 
+function generateDimensions() {
+  let counter = 1;
+  for (let i = 10; i <= 200; i += 10) {
+    dimensions.push(
+      {
+        key: i,
+        widthBar: widthBar / counter,
+        heightBar: heightFactor / counter
+      });
+    counter++;
+  }
+};
+
 function createRandomArray() {
   let array = [...Array(numsOfBars).keys()].map(i => i + 1)
   for (var i = array.length - 1; i > 0; i--) {
@@ -63,9 +82,13 @@ function createRandomArray() {
 };
 
 function renderBars(array: number[]) {
+  let barDimension = dimensions!.find((item: any) => item.key === numsOfBars);
+  heightFactor = barDimension!.heightBar;
+
   for (let i = 0; i < numsOfBars; i++) {
     let bar = document.createElement("div");
     bar.classList.add("bar");
+    bar.style.width = barDimension!.widthBar + "px";
     bar.style.height = array[i] * heightFactor + "px";
     barsContainer?.appendChild(bar);
   }
@@ -77,7 +100,19 @@ function sleep(ms: number) {
   })
 };
 
+function blockButtons(pointer: string, opacity: string) {
+  randomizeArray!.style.pointerEvents = pointer;
+  selectAlgorithm!.style.pointerEvents = pointer;
+  sortBtn!.style.pointerEvents = pointer;
+  slider!.style.pointerEvents = pointer;
+  randomizeArray!.style.opacity = opacity;
+  selectAlgorithm!.style.opacity = opacity;
+  sortBtn!.style.opacity = opacity;
+  slider!.style.opacity = opacity;
+};
+
 async function bubbleSort(array: number[]) {
+  blockButtons("none", "0.5");
   let postionChangesCounter = 0;
   let arrayAccessCounter = 0;
   let bars: any | null = document.getElementsByClassName("bar");
@@ -85,10 +120,13 @@ async function bubbleSort(array: number[]) {
     arrayAccessCounter++;
     for (let j = 0; j < array.length - 1; j++) {
       if (array[j] > array[j + 1]) {
-        comparisons!.textContent = `-Bubble Sort, Switched positions: ${(postionChangesCounter++).toString()}, array accesses: ${(arrayAccessCounter).toString()}`;
+        comparisons!.textContent = `Bubble Sort
+        \nArray length: ${array.length}
+        \nSwitched positions: ${(postionChangesCounter++).toString()}
+        \nArray accesses: ${(arrayAccessCounter).toString()}`;
         for (let k = 0; k < bars!.length; k++) {
           if (k !== j && k !== j + 1) {
-            bars![k].style.backgroundColor = "#dde7ee";
+            bars![k].style.backgroundColor = "#fff";
           }
         }
         let temp = array[j];
@@ -103,5 +141,6 @@ async function bubbleSort(array: number[]) {
     }
     await sleep(speedFactor);
   }
+  blockButtons("auto", "1");
   return array;
-}
+} 
